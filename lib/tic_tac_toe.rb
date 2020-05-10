02aeb22f3e4005269a95d12b033811e1fbb23a23
Dwight Nodolf <dnodolf@gmail.com>
2020-05-10 23:42:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,10 +68,10 @@ class TicTacToe
       WIN_COMBINATIONS.detect(->{false}) do |win_combo|
         if 
           @board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X"
-          win_combo[0,1,2]
+          win_combo
         else
           @board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O"
-          win_combo[0,1,2]
+          win_combo
           
           #binding.pry
         end
