6fedce3da60dedf70b236de8908f19f679e58c82
Dwight Nodolf <dnodolf@gmail.com>
2020-05-11 00:03:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,9 +67,10 @@ class TicTacToe
       
         if 
           @board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X"
-       
-        elsif
+          win_combo
+       elsif
           @board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O"
+          win_combo
         end
       end
       false
